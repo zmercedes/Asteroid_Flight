d45e10d857f555197c23cc4d9b1df40a008cfeb3
zmercedes <zmercedes89@gmail.com>
2017-07-20 00:02:53 -0400

    first commit

---
+++ PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed = 6f;
+	float halfScreenWidth;
+	float halfPlayerWidth;
+
+	void Start () {	
+		halfPlayerWidth = transform.localScale.x / 2f;
+		halfScreenWidth = Camera.main.aspect * Camera.main.orthographicSize;
+	}
+	
+	void Update () {
+		float input = Input.GetAxisRaw("Horizontal");
+		float velocity = input*moveSpeed;
+		transform.Translate(Vector2.right * velocity * Time.deltaTime);
+
+		if(transform.position.x < -halfScreenWidth-halfPlayerWidth)
+			transform.position = new Vector2(halfScreenWidth,transform.position.y);
+		if(transform.position.x > halfScreenWidth+halfPlayerWidth)
+			transform.position = new Vector2(-halfScreenWidth,transform.position.y);
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.gameObject.tag == "asteroid"){
+			Debug.Log("Crash!");
+			Destroy(this.gameObject);
+		}
+	}
+}
