f8ab9b259c724d2263c941d4a4a45b962fe830e6
zmercedes <zmercedes89@gmail.com>
2017-07-22 19:12:59 -0400

    increasing difficulty implemented

--- AsteroidGen.cs
+++ AsteroidGen.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class AsteroidGen : MonoBehaviour {
 
 	public GameObject asteroid;
-	public float secondsBetweenGen = 1f;
-	public float decreasingGenTime = 30;
+	public float secondsBetweenGen = 1.5f;
+	public float decreasingGenTime = 30f;
+	public float decreaseGenBy = .25f;
 	float xCoord;
 	float halfScreenWidth;
 	float halfScreenHeight;
 	float halfPlayerWidth;
 	float halfPlayerHeight;
 	float nextGenTime=0f;
+	float nextDecreaseTime;
 
 	Vector2 position;
 
 	void Start(){
+		nextDecreaseTime = decreasingGenTime;
 		halfScreenWidth = Camera.main.aspect * Camera.main.orthographicSize;
 		halfScreenHeight = Camera.main.orthographicSize;
 		halfPlayerWidth = asteroid.transform.localScale.x /2f;
@@ -27,9 +30,16 @@ public class AsteroidGen : MonoBehaviour {
 	void Update () {
 		if(Time.time > nextGenTime){
 			xCoord = Random.Range(halfPlayerWidth-halfScreenWidth,halfScreenWidth-halfPlayerWidth);
-			position = new Vector2(xCoord,halfScreenHeight+halfPlayerHeight);
+			position = new Vector3(xCoord,halfScreenHeight+halfPlayerHeight,0);
 			Instantiate(asteroid, position, Quaternion.identity, transform);
 			nextGenTime += secondsBetweenGen;
+		} 
+	}
+
+	void FixedUpdate(){
+		if(Time.time == nextDecreaseTime && secondsBetweenGen > .25){
+			secondsBetweenGen -= decreaseGenBy;
+			nextDecreaseTime += decreasingGenTime;
 		}
 	}
 }
